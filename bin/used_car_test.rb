eccfeef64e710aa52e901ef94690716c10fcb4d2
EricRho <eric.rho.87@gmail.com>
2014-06-12 23:54:58 -0400

    Commit used car test file

--- bin/used_car_test.rb
+++ bin/used_car_test.rb
@@ -7,3 +7,7 @@ civic = UsedCar.new("Honda", "Civic SI", 2007, 25_000, 0.3, 100_000)
 
 
 puts "Used #{civic.year} #{civic.make} #{civic.model} with #{civic.mileage} miles."
+
+puts civic.depreciated_value_mile
+
+puts civic.price
