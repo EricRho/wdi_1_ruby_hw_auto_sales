207bae148c4d35bebc26445f30fb72e0d793c809
EricRho <eric.rho.87@gmail.com>
2014-06-13 00:37:25 -0400

    Commit used car test file after creating damage methods

--- bin/used_car_test.rb
+++ bin/used_car_test.rb
@@ -1,6 +1,12 @@
 require_relative '../lib/used_car_class.rb'
+require_relative '../lib/damage'
 
 civic = UsedCar.new("Honda", "Civic SI", 2007, 25_000, 0.3, 100_000)
+mini = UsedCar.new("Mini", "Cooper S", 2004, 26_000, 0.3, 2_000)
+scratch = Damage.new("scratch", 300)
+gear_box = Damage.new("broken gearbox", 1_500)
+
+
 #mini = UsedCar.new("Mini", "Cooper S", 2010, 26_000, 0.3, 100_000)
 
 
@@ -11,3 +17,10 @@ puts "Used #{civic.year} #{civic.make} #{civic.model} with #{civic.mileage} mile
 puts civic.depreciated_value_mile
 
 puts civic.price
+
+
+
+mini.include_damage(gear_box)
+puts mini.price
+
+
