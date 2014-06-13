7b658888bee108fbddc196104ac18326a6d6c431
EricRho <eric.rho.87@gmail.com>
2014-06-12 22:13:33 -0400

    Create directions from HW for guidance

--- lib/used_car_class.rb
+++ lib/used_car_class.rb
@@ -4,3 +4,8 @@ class UsedCar < Car
 
 end
 
+# has the same attributes as a new car
+# has a mileage
+# may have one or more damages (scuffed paint, missing hubcaps, etc.)
+# has a value (same calculation as a new car, modified by mileage depreciation and the cost of all current damages)
+# all used cars have a constant mileage depreciation of 1% per 10,000 miles (0.0001% per mile)
